b0c60557abd413157a1fca8ae523666aa3be173f
y-matsuda <y-matsuda@esm.co.jp>
2013-04-23 10:55:37 +0900

    テストデータ変更

--- src/test/resources/com/change_vision/astah/extension/plugin/csharpreverse/reverser/default_value1/a.cs
+++ src/test/resources/com/change_vision/astah/extension/plugin/csharpreverse/reverser/default_value1/a.cs
@@ -1,11 +1,6 @@
-/// <summary>
-/// AaaƒNƒ‰ƒX‚Ìà–¾
-/// </summary>
 public class Aaa
 {
-    /// <summary>
-    /// à–¾
-    /// </summary>
     static Logger log = InternalTrace.GetLogger(typeof(NUnitTestAgent));
-
 }
+
+public class NUnitTestAgent { }
