cda6485c93b740e111ad4dda82340e3539f1446f
y-matsuda <y-matsuda@esm.co.jp>
2013-05-09 16:44:19 +0900

    テストの追加・修正

--- src/test/java/com/change_vision/astah/extension/plugin/csharpreverse/reverser/InitBeanPropertySetterRuleTest.java
+++ src/test/java/com/change_vision/astah/extension/plugin/csharpreverse/reverser/InitBeanPropertySetterRuleTest.java
@@ -4,6 +4,7 @@
 package com.change_vision.astah.extension.plugin.csharpreverse.reverser;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -18,11 +19,26 @@ public class InitBeanPropertySetterRuleTest {
 	 * のためのテスト・メソッド。
 	 */
 	@Test
-	public void testReplaceInvalidEnd_() {
+	public void testReplaceInvalidEnd_refタグ以外が削除されること() {
 		InitBeanPropertySetterRule rule = new InitBeanPropertySetterRule();
-		String currentContent = "<ref a a>a</sp>c</ref><ref a a>a</sp>c</ref>b";
+		String currentContent = "<ref a a>a<sp />c</ref><ref a a>a<sp />c</ref>b";
 		assertEquals("<ref a a>a c</ref><ref a a>a c</ref>b",
 				rule.replaceInvalidEnd(currentContent));
+	}
+
+	/**
+	 * {@link com.change_vision.astah.extension.plugin.csharpreverse.reverser.InitBeanPropertySetterRule#replaceInvalidEnd(String)}
+	 * のためのテスト・メソッド。
+	 */
+	@Test
+	public void testReplaceInvalidEnd_タグの後ろの括弧が見つからなくてもスタックオーバーフローしないこと() {
+		InitBeanPropertySetterRule rule = new InitBeanPropertySetterRule();
+		String currentContent = "<a a aac< a aacb";
+		try {
+			rule.replaceInvalidEnd(currentContent);
+		} catch (StackOverflowError e) {
+			fail();
+		}
 	}
 
 	/**
@@ -32,9 +48,24 @@ public class InitBeanPropertySetterRuleTest {
 	@Test
 	public void testReplaceRefClassname_refタグがすべて削除されること() {
 		InitBeanPropertySetterRule rule = new InitBeanPropertySetterRule();
-		String currentContent = "<ref a a>a</sp>c</ref><ref a a>a</sp>c</ref>b";
-		assertEquals("a</sp>ca</sp>cb",
+		String currentContent = "<ref a a>a<sp />c</ref><ref a a>a<sp />c</ref>b";
+		assertEquals("a<sp />ca<sp />cb",
 				rule.replaceRefClassname(currentContent));
+	}
+
+	/**
+	 * {@link com.change_vision.astah.extension.plugin.csharpreverse.reverser.InitBeanPropertySetterRule#replaceRefClassname(String)}
+	 * のためのテスト・メソッド。
+	 */
+	@Test
+	public void testReplaceRefClassname_refタグの後ろの括弧が見つからなくてもスタックオーバーフローしないこと() {
+		InitBeanPropertySetterRule rule = new InitBeanPropertySetterRule();
+		String currentContent = "<ref a aac<ref a aacb";
+		try {
+			rule.replaceRefClassname(currentContent);
+		} catch (StackOverflowError e) {
+			fail();
+		}
 	}
 
 }
